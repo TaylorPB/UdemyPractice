c8a102d9c80bddc1fa06100c853d534b96b88f00
OU IT <43416443+TaylorPB@users.noreply.github.com>
2021-07-23 10:28:26 -0400

    Update EvenDigitSum.java
    Added some comments.

--- EvenDigitSum.java
+++ EvenDigitSum.java
@@ -5,28 +5,30 @@ public class EvenDigitSum {
 
     public static int getEvenDigitSum(int number) {
 
-        int even = 0;
+        int even = 0; //creating the values fir both even and last integer  needed.
         int last = 0;
 
 
-        if (number < 0) {
+        if (number < 0) {        //If statement so user does not include any negative values. 
 
             System.out.println(" Invalid Value");
-            return -1;
+            return -1;    //Code will end here if the value is less than 0.
 
 
         }
 
-        for (int i = 0; number != 0; number /= 10) {
+        for (int i = 0; number != 0; number /= 10) {   //for loop that was created.   Used the basic i  for index , number will not equal zero !=0 and number /= will result in the results for number after done. 
+            
+            
 
 
-            last = number % 10;
+            last = number % 10;   //will get the remainder once number is divided by 10. 
 
 
-            if (last % 2 == 0) {
+            if (last % 2 == 0) {    // If the value is not able to be divided by two then it's not a even value.
 
 
-                even += last;
+                even += last; 
 
 
             }
