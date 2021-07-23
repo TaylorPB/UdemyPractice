64d07b1081722da826f6f00e9255d0bacd859d72
OU IT <43416443+TaylorPB@users.noreply.github.com>
2021-07-23 13:22:10 -0400

    Update FactorPrinter.java

--- FactorPrinter.java
+++ FactorPrinter.java
@@ -1,9 +1,9 @@
 public class FactorPrinter {
     public static void printFactors(int number) {
 
-        int factorthingy = 0;
+        
 
-        if (number < 1) {
+        if (number < 1) { //Setting anything less than 1 is unneeded. 
 
             System.out.println("Invalid Value");
 
@@ -11,10 +11,11 @@ public class FactorPrinter {
         } else if (number >= 1) {
 
 
-            for (int i = 1; i <= number; i++) {
+            for (int i = 1; i <= number; i++) { //For loop that will display everything from 1-6. Started i off at 1 so  0 will not be added.  
 
 
-                if (number % i == 0)
+                if (number % i == 0) //if number and i rendered no remainders then i will be displayed. I.e. 9 =1 ,3, 9 
+                    
 
                     System.out.println(i);
 
