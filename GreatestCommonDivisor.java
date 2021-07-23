a567e3f8c2848b22ed2318f3d40f86b64c7e8ac8
OU IT <43416443+TaylorPB@users.noreply.github.com>
2021-07-23 12:24:55 -0400

    Update GreatestCommonDivisor.java

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -3,9 +3,13 @@ public class GreatestCommonDivisor {
 
 
     public static int getGreatestCommonDivisor(int first, int second) {
+        // first and second are the parameters  
+        
 
 
-        if (first < 10 || second < 10) {
+        // If the value is less than 10 it will result in a invalid value. 
+        
+        if (first < 10 || second < 10) { // | | operator  first or second can not be less 10 
 
 
             System.out.println("Invalid Value");
@@ -13,13 +17,17 @@ public class GreatestCommonDivisor {
 
         } else {
 
-
+// GRC = Greatest commeon Denominator , but GRC was my onld job.
             int grc = 1;
 
 
             System.out.println(first + " " + second + " ");
+            
+            //Basic for loop 
             for (int i = 1; i <= first && i <= second; i++) {
 
+                
+                //Remainders need to equal 0 to make sure there is no remainder. 
                 if ((first % i == 0) && (second % i == 0))
 
                     grc = i;
@@ -40,7 +48,7 @@ public class GreatestCommonDivisor {
 
 
 
-
+//basic print method. 
 
     public static void main(String args[]){
 
